a74f933ab096bd6bcb579193586f136fabbd5f51
gjw <122486975@qq.com>
2017-08-15 00:14:00 +0800

    go on edit page

--- src/components/edit/edit.vue
+++ src/components/edit/edit.vue
@@ -1,6 +1,21 @@
 <template>
-  <div id="edit">
+  <div class="edit">
       <!-- bidirectional data binding（双向数据绑定） -->
+    <div class="head">
+      <div class="title">
+        <label for="topicTitle">帖子标题</label>
+        <input type="text" placeholder="发表新话题">
+      </div>
+      <div class="type">
+        <h5 @click="showDrop">请选择</h5>
+        <ul class="dropdown" v-show="classify" @click.stop.prevent="select($event)">
+          <li>分享</li>
+          <li>问答</li>
+          <li>招聘</li>
+          <li>测试</li>
+        </ul>
+      </div>
+    </div>
 	  <quill-editor v-model="content"
 	                ref="myQuillEditor"
 	                :options="editorOption"
@@ -15,12 +30,19 @@
   export default {
     data () {
       return {
-        content: '<h2>I am Example</h2>',
+        classify: false,
+        content: '',
         editorOption: {
           // some quill options
           modules: {
             toolbar: [
-              ['bold', 'italic']
+              [{size: ['small', false, 'large', 'huge']}],
+              [{font: []}],
+              ['bold', 'italic'],
+              ['blockquote', 'code-block'],
+              [{list: 'ordered'}, {list: 'bullet'}],
+              [{color: []}, {background: []}],
+              ['image']
             ]
           }
         }
@@ -30,18 +52,27 @@
     // 如果需要手动控制数据同步，父组件需要显式地处理changed事件
     methods: {
       onEditorBlur (editor) {
-        console.log('editor blur!', editor)
+        // console.log('editor blur!', editor)
       },
       onEditorFocus (editor) {
-        console.log('editor focus!', editor)
-        console.log(this.editorOption)
+        // console.log('editor focus!', editor)
       },
       onEditorReady (editor) {
-        console.log('editor ready!', editor)
+        // console.log('editor ready!', editor)
       },
       onEditorChange ({ editor, html, text }) {
-        console.log('editor change!', editor, html, text)
+        // console.log('editor change!', editor, html, text)
         this.content = html
+      },
+      showDrop () {
+        this.classify = !this.classify
+      },
+      select (e) {
+        if (e.target.nodeName === 'LI') {
+          let val = e.target.innerHTML
+          e.target.parentNode.previousElementSibling.innerHTML = val
+          this.classify = !this.classify
+        }
       }
     },
     // get the current quill instace object.
@@ -52,12 +83,52 @@
     },
     mounted () {
       // you can use current editor object to do something(quill methods)
-      console.log('this is current quill instance object', this.editor)
+      // console.log('this is current quill instance object', this.editor)
     }
   }
 </script>
 
 <style lang="stylus">
-	.edit
-		width 100%
+  .edit
+    position absolute
+    top 40px
+    left 0
+    bottom 0
+    width 100%
+    background #fff
+    .head
+      .title
+        text-align left
+        label
+          padding-left 5px
+          font-size 16px
+          line-height 30px
+        input
+          width 100%
+          height 20px
+          font-size 14px
+          margin 0 5px
+          border none
+          border-bottom 1px solid #80bd01
+          outline none
+      .type
+        position relative
+        width 100%
+        height 30px
+        padding-left 5px
+        margin-bottom 5px
+        h5
+          font-size 16px
+          line-height 30px
+          font-weight 600
+        .dropdown
+          position absolute
+          top 30px
+          left 0
+          width 100%
+          background #fff
+          z-index 1300
+          li
+            padding-left 5px
+            line-height 30px
 </style>
